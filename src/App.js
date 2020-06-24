84a099ac8cb191a27c780e7d73a6ee0bc94e8800
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-24 10:39:42 -0300

    added two components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import GraphicDesign from "./GraphicDesign";
+import AboutMe from "./AboutMe";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Switch>
+        <Route exact path="/" component={AboutMe} />
+        <Route exact path="/graphic-design" component={GraphicDesign} />
+      </Switch>
     </div>
   );
 }
---
+++ src/GraphicDesign.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function GraphicDesign() {
+  return <h1>Graphic Design</h1>;
+}

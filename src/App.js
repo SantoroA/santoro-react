38cc9596e04d46b05913c2414081bdf02c779e04
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-24 10:31:55 -0300

    added redirects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Nav from "./Nav";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <h1>Hello World</h1>
     </div>
   );
 }

bcc9f4fe33ea551800a3fbc4f60ce0db225964a4
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-28 13:31:42 -0300

    about image
    added an imahe to my about page

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Title from "./Containers/Title";
-
+import coder from "./imgs/coder.svg";
 import Nav from "./Nav";
 import "./About.css";
 export default function AboutMe() {
   return (
-    <div className="About container-flex">
+    <div className="About container">
       <Nav fixedTop="true" />
 
       <Title>
         <h1>About Me</h1>
       </Title>
+      <row>
+        <img className="mt-4" src={coder} alt="coder" />
+      </row>
     </div>
   );
 }

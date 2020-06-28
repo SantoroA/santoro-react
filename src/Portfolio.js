a85a837a2e432ce6e53ae9852201938567d89434
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-28 13:17:13 -0300

    added an item in portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Title from "./Containers/Title";
 import Nav from "./Nav";
+import weatherApp from "./imgs/weatherApp.svg";
+import "./Portfolio.css";
 export default function Portfolio() {
   return (
-    <div className="container">
+    <div className="container Portfolio">
       <Nav fixedTop={true} />
       <Title>
         <h1>My Portfolio</h1>
       </Title>
+      <div className="row mt-5">
+        <div className="col-3">
+          <a
+            href="https://suspicious-brattain-a20b89.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="weather-app" src={weatherApp} />
+          </a>
+          <h3 className="mt-4">ReactJS Weather App</h3>
+        </div>
+      </div>
     </div>
   );
 }

afc2538d2f49d1dc3f1d5b296b416ea5d4f727e7
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-28 17:11:08 -0300

    styling changes

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./Containers/Title";
 import coder from "./imgs/coder.svg";
 import Nav from "./Nav";
 import "./About.css";
 export default function AboutMe() {
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
     <div className="About container">
       <Nav fixedTop="true" />
@@ -14,16 +15,42 @@ export default function AboutMe() {
       <div className="row mt-5">
         <div className="col-md-6">
           <img src={coder} alt="coder" />
-          <p className="description px-2">
-            I'm an aspiring Front-End React-JS Developer. I may not have a lot
-            of experience in coding, but I do have tons of experience in the
-            real world. I've worked as an architect, as a Professor, as a
-            freelance graphic designer... I also have a specialization and a
-            masters degree in structure... However, I gave it all up when I fell
-            in love with coding! Recently finished all SheCodes courses
-            (SheCodes Plus and SheCodes React) and am looking for an opportunity
-            to expand my coding knowledge while working closely with designers.
-          </p>
+          <div className="description px-5">
+            <h2 className="my-4">This is me!</h2>
+            {isExpanded ? (
+              <p>
+                The day I fell in love with coding changed my whole life. I am
+                an aspiring Front-End React-JS Developer with a bachelors in
+                Architecture and a master in structure. I have lived and studied
+                internationally including a year of Architecture in London in
+                2013, 3 months of Interior Design in Milan, and a specilization
+                in Structure and Foundations in Brazil. I am fluent in
+                Portuguese, English and Italian. <br /> My experience as an
+                architect, professor, and graphic designer have led me to
+                pursuing a career in coding. I recently finished all SheCodes
+                courses (SheCodes Plus and SheCodes React) and am looking for an
+                entry-level career opportunity to expand my coding knowledge
+                while working closely with other designers.{" "}
+                <button
+                  className="readMoreLink"
+                  onClick={() => setIsExpanded(false)}
+                >
+                  Collapse
+                </button>
+              </p>
+            ) : (
+              <p>
+                I am an aspiring Front-End React-JS Developer with a bachelors
+                in Architecture and a master in structure.
+                <button
+                  className="readMoreLink"
+                  onClick={() => setIsExpanded(true)}
+                >
+                  Read More
+                </button>{" "}
+              </p>
+            )}
+          </div>
         </div>
         <div className="col-md-6">
           <p>Qualifications: in construction</p>
